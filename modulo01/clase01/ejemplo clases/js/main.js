1b43cd07d7968cd05eb30b2092555ba58e19e580
asdad <jorriccelli1986@gmail.com>
2019-07-23 22:01:56 -0500

    clase01

---
+++ modulo01/clase01/ejemplo clases/js/main.js
@@ -0,0 +1,29 @@
+
+/*
+var num1=prompt("Ingresa num1");
+var num2=prompt("Ingresa num2");
+
+var suma=(num1)+(num2); // adición 
+console.log(suma);
+num3="10";
+num4="20";
+var suma2="10"+"20";  // concatenación
+console.log(suma2);
+
+
+*/
+
+var maximo=Math.max(9,94,495,67);
+var minimo=Math.min(10,30,405,4);
+var pi=Math.PI;
+var potencia=Math.pow(3,4); // 81
+var sqrt=Math.sqrt(169); // 13
+var redondeo1=Math.round(10.4); // 10
+var redondeo2=Math.round(10.7); // 11
+var red_superior=Math.ceil(10.1); // 11
+var red_inferior=Math.floor(10.9) // 10
+var absoluto=Math.abs(-10) //
+var aleatorio=Math.random()*10;
+var aleatorio_redondeado=Math.round(aleatorio);
+console.log(aleatorio_redondeado);
+
---
+++ modulo01/clase01/variables/js/main.js
@@ -0,0 +1,105 @@
+console.log("estamos ejecutando código desde JS");
+console.log(123);
+
+var dato1; 
+var dato2;
+// var datos persona; // no aplica
+// var 12123; //no aplica
+
+/*
+var datos_persona;
+dato1="Jc";
+dato1 
+"dato1"
+*/
+
+
+console.log(dato1);
+dato1="Juan Carlos";
+
+console.log(dato1);
+console.log("dato1");
+
+
+var num1=1;
+var num2=0;
+
+var num3;
+var num4;
+num3=num1;
+num4=num3;
+
+var nombre;
+nombre="Juan Carlos";
+
+var nombre="Juan Carlos";
+var codigo=1234543;
+// nombre;
+// edad;
+// correo;
+// telefono;
+
+
+var nombre="JC"
+var edad=32;
+var correo="jtoricce@gmail.com";
+var telefono=987903945;
+var telefono="987903945";
+console.log(num4)  // "num3"
+
+
+//* operacicones con variables **/
+/* 
+ + suma
+ - resta
+ * multiplicacion
+ / division
+ % Modular
+*/
+var num_1;
+var num_2;
+var suma=num_1+num_2;
+console.log(suma);
+console.log(num_1+num_2);
+num_1=0
+console.log(num_1+num_2);
+console.log(suma);
+console.log(num_1+num_2);
+console.log(suma);
+
+
+
+var texto=10;
+texto
+texto="hola";
+texto
+
+/*** variables bool ***/
+
+
+var estado;
+estado=true; // valor mayor 0 o texto
+estado=false;  //-1 , null , undifined , NaN
+estado="123"
+
+/** var fecha **/
+
+var fecha="23/07/2019";
+var nueva_fecha=new Date();
+
+console.log(fecha);
+console.log(nueva_fecha);
+
+
+
+
+
+
+
+
+
+
+
+
+
+

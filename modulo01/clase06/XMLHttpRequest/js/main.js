7ecfc4144140491316c59e234ab25c8c9fd44de0
jtorriccelli1986@gmail.com <sergiohidalgocaceres@gmail.com>
2019-08-08 20:05:35 -0500

    update

---
+++ modulo01/clase06/XMLHttpRequest/js/main.js
@@ -0,0 +1,71 @@
+
+
+let xhr=new XMLHttpRequest();  //readyState 0
+
+let url ="https://reqres.in/api/users?page=1";
+
+xhr.open("GET",url,true);   //readeyState 1
+xhr.send();
+
+            // readyState 2. // 20seg
+xhr.onreadystatechange=function(){
+	let estado=xhr.readyState;
+	if(estado==4){
+		if(xhr.status==200){
+			console.log(xhr);
+			let resultado=JSON.parse(xhr.response);
+			console.log(resultado);	
+			// yo estoy seguro que tengo respuesta
+		}
+		
+		
+	}
+	//console.log(xhr.responseText);
+}
+//console.log(xhr);  // readyState 4;
+
+
+
+console.log("hola");
+
+
+
+
+
+
+/*
+console.log(xhr);
+
+console.log(xhr.readyState);
+console.log(xhr.responseText); // aquí esta la info de respuesta;
+console.log(xhr.status); // información acerca de la petición;
+*/
+
+
+/*
+
+class Persona{
+	nombre,
+	apellido,
+	edad;
+
+	correr(){
+		//tareas
+	}
+	dormir(){
+		// tareas
+	}
+}
+
+
+let elemento1=new Persona();
+
+elemento1.nombre;
+elemento1.apellido;
+elemento1.edad;
+elemento1.correr();
+elemento1.dormir();
+
+*/
+
+
